f7d573a98d760f85d28508e5f49270476f03d281
Edwinsfc03e <110621526+Edwinsfc03e@users.noreply.github.com>
2022-08-18 12:04:10 -0500

    Update Country.php

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -7,6 +7,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Country extends Model
 {
+    protected $table = "Continents";
+    protected $primaryKey = "continet_id";
+    
+    public function region(){
+        return $this-> belongsto(Region::class,
+                                    "continent_id");                               
+          
+    }
      public function idiomas(){
         return $this-> belongsToMany(Language::class,"country_languages",
                                                          "country_id",
